9ec09fe3e4efbe822fc600f96dcbc3210693c296
unknown <babacanogun@gmail.com>
2019-12-27 18:48:41 +0300

    feat: Generate public and private VAPID keys and subscribe the user for the push.

--- client/app/js/PushService.js
+++ client/app/js/PushService.js
@@ -1,28 +1,37 @@
+import { getPublicApplicationKey } from "./utils";
+
 export default class PushService {
     constructor(serviceWorker) {
         this.serviceWorker = serviceWorker;
     }
 
-     askNotificationPermission() {
-        // The API for getting notification permission is changed from taking a callback as an argument to taking promise.
+     async askNotificationPermission() {
+        // The API for getting notification permission has changed from taking a callback as an argument to taking promise.
         // We need to address both situations.
-        return new Promise((resolve, reject) => {
-            const permissionResult = Notification.requestPermission(function (result) {
-                resolve(result);
-            });
+        const permissionResult = await new Promise((resolve, reject) => {
+             const permissionResult = Notification.requestPermission(function (result) {
+                 resolve(result);
+             });
 
-            if (permissionResult) {
-                permissionResult.then(resolve, reject);
-            }
-        })
-        .then(function (permissionResult) {
-            if (permissionResult !== 'granted') {
-                throw new Error('We weren\'t granted permission.');
-            }
-        });
+             if (permissionResult) {
+                 permissionResult.then(resolve, reject);
+             }
+         });
+
+         if (permissionResult !== 'granted') {
+             throw new Error('We weren\'t granted permission.');
+         }
+
+         return permissionResult;
     };
 
-    subscribeUser() {
-        console.log(this.serviceWorker);
+    async subscribeUser() {
+        const subscribeOptions = {
+            userVisibleOnly: true,
+            applicationServerKey: getPublicApplicationKey(),
+        };
+
+        const subscription = await this.serviceWorker.pushManager.subscribe(subscribeOptions);
+        console.log(JSON.stringify(subscription));
     }
 }
--- client/app/js/utils.js
+++ client/app/js/utils.js
@@ -10,3 +10,26 @@ export async function registerServiceWorker() {
         throw new Error('This demo requires a browser that supports service workers.');
     }
 };
+
+export function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
+export function getPublicApplicationKey() {
+    return urlBase64ToUint8Array(process.env.PUBLIC_KEY);
+}
+
+export function getPrivateApplicationKey() {
+    return urlBase64ToUint8Array(process.env.PRIVATE_KEY);
+}
